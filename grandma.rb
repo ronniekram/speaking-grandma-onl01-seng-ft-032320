3b8c56b5341a4da1967069844c518e32a6e9d08c
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-23 20:26:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,4 +16,8 @@
 def speak_to_grandma(string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU, GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
